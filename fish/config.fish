2438513d0c0502284d447bbfd7e7fc3e04c0351f
Paulemberg <paulemberg@localhost.localdomain>
2019-12-29 02:16:05 -0300

    adding fish config

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+#bin/bash
+
+# Aliases for Git
+alias gadd="git add ."
+alias lg1="git log --graph --abbrev-commit --decorate --date=relative --all"
+alias lg2="git log --graph --oneline --decorate --all"
+alias lg3="git log --graph --pretty=format:'%C(bold blue)%h%Creset - %C(yellow)%d%Creset %s %C(cyan dim)(%cr)%Creset %n' --abbrev-commit --branches"
+
+## NPM aliases
+alias npmr="npm run start"
+alias npmrb="npm run build"
+
+## Python aliases
+alias pip="pip3"
